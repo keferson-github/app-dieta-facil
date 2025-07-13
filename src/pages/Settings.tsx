0fdada99751bc48d8c219e8a4ac5f89a8c58c0c6
Keferson Cola Da Conceicao <123316365+keferson-github@users.noreply.github.com>
2025-07-12 23:42:37 -0300

    Ocultação do logotipo da página de Configurações, somente na versão Mobile.

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -168,7 +168,7 @@ const Settings = () => {
             </div>
             
             <div className="flex items-center space-x-4">
-              <div className="flex items-center space-x-2">
+              <div className="hidden sm:flex items-center space-x-2">
                 <div className="w-8 h-8 health-gradient rounded-lg flex items-center justify-center">
                   <Apple className="w-5 h-5 text-white" />
                 </div>
